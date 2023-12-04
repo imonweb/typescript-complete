80188a1c85b007c1984505fe45a1aabef16bbed9
Imon <contact@imonweb.co.uk>
2023-12-04 16:21:07 +0000

    arrays & objects

--- lesson3/build/js/main.js
+++ lesson3/build/js/main.js
@@ -1,20 +1,62 @@
 "use strict";
-let myName = 'Peter';
-let meaningOfLife;
-let isLoading;
-// let album: any;
-/* union type */
-let album;
-myName = 'John';
-meaningOfLife = 42;
-isLoading = true;
-// album = 1984
-album = 'Van Halen';
-const sum = (a, b) => {
-    return a + b;
+/* ====== Arrays & Objects ====== */
+/*  3 types of array */
+let stringArr = ['one', 'hey', 'John'];
+let guitars = ['strat', 'Les Paul', 5150];
+let mixedData = ['EVH', 1984, true];
+stringArr[0] = 'Jack';
+stringArr.push('hey');
+guitars[0] = 1984;
+guitars.unshift('Jim');
+// guitars = stringArr 
+// mixedData = guitars 
+// guitars = mixedData
+let test = [];
+let bands = [];
+bands.push('Van Halen');
+// Tuple
+let myTuple = ['Jake', 44, true];
+let mixed = ['John', 1, false];
+// mixed = myTuple
+// myTuple = mixed
+myTuple[1] = 44;
+/*  Objects */
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: 'Dave',
+    prop2: true,
 };
-let postId;
-let isActive;
-// regular expression
-let re = /\w+/g;
-console.log(sum);
+exampleObj.prop1 = 'johnny';
+let evh = {
+    name: 'Eddie',
+    // active: false,
+    active: true,
+    albums: [1984, 5150, 'OU812']
+};
+let jp = {
+    name: 'Jimmy',
+    active: true,
+    albums: ['I', 'II', 'IV']
+};
+// evh = jp
+const greetGuitarist = (guitarist) => {
+    if (guitarist.name) {
+        return `Hello ${guitarist.name.toUpperCase()}!`;
+    }
+    return 'Hello!';
+};
+console.log(greetGuitarist(jp));
+// enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
--- lesson3/src/main.ts
+++ lesson3/src/main.ts
@@ -1,25 +1,95 @@
-let myName: string = 'Peter';
-let meaningOfLife: number;
-let isLoading: boolean;
-// let album: any;
+/* ====== Arrays & Objects ====== */
 
-/* union type */
-let album: string | number;
+/*  3 types of array */
+let stringArr = ['one', 'hey', 'John']
 
-myName = 'John'
-meaningOfLife = 42
-isLoading = true
-// album = 1984
-album = 'Van Halen'
+let guitars = ['strat', 'Les Paul', 5150]
 
-const sum = (a: number, b: string) => {
-  return a + b
+let mixedData = ['EVH', 1984, true]
+
+stringArr[0] = 'Jack'
+stringArr.push('hey')
+
+guitars[0] = 1984
+guitars.unshift('Jim')
+
+// guitars = stringArr 
+// mixedData = guitars 
+// guitars = mixedData
+
+let test = []
+let bands: string[] = []
+bands.push('Van Halen')
+
+// Tuple
+let myTuple: [string, number, boolean] = ['Jake', 44, true]
+
+let mixed = ['John', 1, false]
+// mixed = myTuple
+
+// myTuple = mixed
+myTuple[1] = 44
+
+
+/*  Objects */
+
+let myObj: object 
+myObj = [] 
+console.log(typeof myObj)
+myObj = bands 
+myObj = {}
+
+const exampleObj = {
+  prop1: 'Dave',
+  prop2: true,
 }
 
-let postId: string | number 
-let isActive: number | boolean
+exampleObj.prop1 = 'johnny'
 
-// regular expression
-let re: RegExp = /\w+/g
+// type Guitarist = {
+//   name: string,
+//   active?: boolean,
+//   albums: (string | number)[]
+// }
 
-console.log(sum)
+interface Guitarist {
+  name?: string,
+  // active?: boolean,
+  active: boolean,
+  albums: (string | number)[]
+}
+
+let evh: Guitarist = {
+  name: 'Eddie',
+  // active: false,
+  active: true,
+  albums: [1984, 5150, 'OU812']
+}
+
+let jp: Guitarist = {
+  name: 'Jimmy',
+  active: true,
+  albums: ['I', 'II', 'IV']
+}
+
+// evh = jp
+
+const greetGuitarist = (guitarist: Guitarist) => {
+  if (guitarist.name){
+    return `Hello ${guitarist.name.toUpperCase()}!`
+  }
+  return 'Hello!'
+}
+
+console.log(greetGuitarist(jp))
+
+// enums
+enum Grade {
+  U = 1,
+  D,
+  C,
+  B, 
+  A,
+}
+
+console.log(Grade.U)
